dd8f523f1bf2c748589093993433160cf0ddb23d
mrondon <mrondon-externo@cnmc.es>
2020-03-01 16:58:01 +0100

    29. Interfaz visual para enviar mensajes

--- 02-basico/src/app/app.component.ts
+++ 02-basico/src/app/app.component.ts
@@ -1,24 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { WebsocketService } from './services/websocket.service';
-import { ChatService } from './services/chat.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
   constructor(
-    public wsService: WebsocketService,
-    public chatService: ChatService
+    public wsService: WebsocketService
   ) {}
-
-  ngOnInit() {
-
-    this.chatService.sendMessage('Hola desde Angular');
-
-  }
 
 }
--- 02-basico/src/app/app.module.ts
+++ 02-basico/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { environment } from '../environments/environment';
 
 // Sockets
@@ -9,14 +11,17 @@ const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     SocketIoModule.forRoot(config)
   ],
   providers: [],

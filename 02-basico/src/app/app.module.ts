7a8eef176a6af5ca398a384e104aae63718f596d
mrondon <mrondon-externo@cnmc.es>
2020-03-01 16:05:23 +0100

    25. Estado del servidor de sockets desde el cliente

--- 02-basico/src/app/app.module.ts
+++ 02-basico/src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
+
+// Sockets
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +14,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]

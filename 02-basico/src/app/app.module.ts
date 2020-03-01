824662dafd5bc83bd8c916d6cac89d4c5b4952e7
mrondon <mrondon-externo@cnmc.es>
2020-03-01 16:12:59 +0100

    26. Interfaz visual para indicar cuando hay conexion al servidor de sockets

--- 02-basico/src/app/app.module.ts
+++ 02-basico/src/app/app.module.ts
@@ -8,10 +8,12 @@ import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

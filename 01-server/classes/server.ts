72487595f6831d95d6ae1a3b9cb7e71e1fd2ca5f
mrondon <mrondon-externo@cnmc.es>
2020-03-01 16:25:11 +0100

    27. Detectar la desconexion de un usuario - Servidor

--- 01-server/classes/server.ts
+++ 01-server/classes/server.ts
@@ -3,6 +3,8 @@ import { SERVER_PORT } from '../global/environment';
 import socketIO from 'socket.io';
 import http from 'http';
 
+import * as socket from '../sockets/socket';
+
 export default class Server {
 
     private static _instance: Server;
@@ -40,6 +42,9 @@ export default class Server {
         this.io.on('connection', cliente => {
 
             console.log('Cliente conectado');
+
+            // Desconectar
+            socket.desconectar( cliente );
 
         });
 
--- 01-server/dist/classes/server.js
+++ 01-server/dist/classes/server.js
@@ -2,11 +2,19 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const environment_1 = require("../global/environment");
 const socket_io_1 = __importDefault(require("socket.io"));
 const http_1 = __importDefault(require("http"));
+const socket = __importStar(require("../sockets/socket"));
 class Server {
     constructor() {
         this.app = express_1.default();
@@ -22,6 +30,8 @@ class Server {
         console.log('Escuchando conexiones - sockets');
         this.io.on('connection', cliente => {
             console.log('Cliente conectado');
+            // Desconectar
+            socket.desconectar(cliente);
         });
     }
     start(callback) {

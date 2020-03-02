8b871df67f135fe1a42d332219d81b5e57a46dff
mrondon <mrondon-externo@cnmc.es>
2020-03-02 13:53:15 +0100

    36. Inicio del proyecto - seccion 5

--- 02-basico/src/app/app.module.ts
+++ 02-basico/src/app/app.module.ts
@@ -12,12 +12,18 @@ const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ChatComponent } from './components/chat/chat.component';
+import { ListaUsuariosComponent } from './components/lista-usuarios/lista-usuarios.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MensajesComponent } from './pages/mensajes/mensajes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    ChatComponent
+    ChatComponent,
+    ListaUsuariosComponent,
+    LoginComponent,
+    MensajesComponent
   ],
   imports: [
     BrowserModule,

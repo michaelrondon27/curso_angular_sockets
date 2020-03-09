598870b0a0603a303de585881bb6e3827b5ce4a3
mrondon <mrondon-externo@cnmc.es>
2020-03-09 11:35:42 +0100

    64. Socket - actualizar la grafica mediante sockets

--- 04-grafica/src/app/components/grafica/grafica.component.ts
+++ 04-grafica/src/app/components/grafica/grafica.component.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Label } from 'ng2-charts';
 import { ChartDataSets } from 'chart.js';
 
+import { WebsocketService } from '../../services/websocket.service';
+
 @Component({
   selector: 'app-grafica',
   templateUrl: './grafica.component.html',
@@ -17,18 +19,27 @@ export class GraficaComponent implements OnInit {
   public lineChartLabels: Label[] = ['Enero', 'Febrero', 'Marzo', 'Abril'];
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    public wsService: WebsocketService
   ) { }
 
   ngOnInit(): void {
 
     this.getData();
 
+    this.escucharSocket();
+
   }
 
   getData() {
 
     this.http.get('http://localhost:5000/grafica').subscribe( (data: any) => this.lineChartData = data );
+
+  }
+
+  escucharSocket() {
+
+    this.wsService.listen( 'cambio-grafica' ).subscribe( (data: any) => this.lineChartData = data );
 
   }
 

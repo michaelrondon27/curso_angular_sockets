72a5210e1c9aa5900d3bc4c934b6cd80a4ab0078
mrondon <mrondon-externo@cnmc.es>
2020-03-09 11:10:09 +0100

    62. Backend - crear una clase para controlar la grafica

---
+++ 03-socket-server-multi/classes/grafica.ts
@@ -0,0 +1,38 @@
+export class GraficaData {
+
+    private meses: string[] = ['enero', 'febrero', 'marzo', 'abril'];
+
+    private valores: number[] = [1, 2, 3, 4];
+
+    constructor() {}
+
+    getDataGrafica() {
+
+        return [
+            {
+                data: this.valores,
+                label: 'Ventas'
+            }
+        ];
+
+    }
+
+    incrementarValor( mes: string, valor: number ) {
+
+        mes = mes.toLocaleLowerCase().trim();
+
+        for ( let i in this.meses ) {
+
+            if ( this.meses[i] === mes ) {
+
+                this.valores[i] += valor;
+
+            }
+
+        }
+
+        return this.getDataGrafica();
+
+    }
+
+}

e55f32f210363439b4db1b67eaad80f3820f4fca
mrondon <mrondon-externo@cnmc.es>
2020-03-26 16:51:18 +0100

    87. Conectar nuestra aplicacion de angular con nuestro servidor de sockets

--- 06-mapbox/src/app/app.module.ts
+++ 06-mapbox/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MapaComponent } from './components/mapa/mapa.component';
 
+import { SocketIoModule } from 'ngx-socket-io';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent,
     MapaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot( environment.socketConfig )
   ],
   providers: [],
   bootstrap: [AppComponent]
